4828ac9e98c04edc0fac25c3c903105bd56ce09c
miguelrock22 <miguelrock222@gmail.com>
2016-11-19 21:50:05 -0500

    themes

--- wp-content/themes/gnosi/header.php
+++ wp-content/themes/gnosi/header.php
@@ -2,8 +2,33 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <title><?php wp_title(); ?> - Gnosi</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title><?php echo wp_title(); ?> - Gnosi</title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <link href="styles.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/tether.min.js"></script>
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
     </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 offset-md-8">
+                        <ul class="nav nav-inline">
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Eventos</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Noticias</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Merchandising</a>
+                          </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
---
+++ wp-content/themes/gnosi/page-noticias.php
@@ -0,0 +1,6 @@
+<?php
+get_header();
+the_title();
+echo get_the_content();
+echo the_post();
+?>
