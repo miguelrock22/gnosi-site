90819af0bfd0fc3406e3b6ddcf73e5f268cf42e6
miguelrock22 <miguelrock222@gmail.com>
2016-11-20 01:09:13 -0500

    news page

--- wp-content/themes/gnosi/page-noticias.php
+++ wp-content/themes/gnosi/page-noticias.php
@@ -6,14 +6,31 @@ get_header();
         <h1><?php echo strtoupper(get_the_title()); ?></h1>
     </div>
     <div class="row">
-        <?php 
-            $loop = new WP_Query( 'cat 2' );
+        <?php
+            $cont = 1;
+            $loop = new WP_Query( array('cat' => 2) );
         ?>
         <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-            <div class="card-news col-md-12">
-                <p><?php the_title(); ?></p>
-                <p><?php the_content(); ?></p>
-            </div>
+            <?php if($cont <= 3 ) : ?>
+                <div class="card-news col-md-4 col-sm-4">
+                    <div class="card card-block">
+                        <h4 class="card-title"><?php the_title(); ?></h4>
+                        <p class="card-text"><?php echo get_field("short_desc"); ?></p>
+                        <a href="javascript:void(0)" class="card-link"><?php echo get_the_date(); ?></a>
+                        <a href="<?php the_permalink(); ?>" class="card-link">Ver más.</a>
+                    </div>
+                    <?php $cont++; ?>
+                </div>
+            <?php else:?>
+                <div class="card-news col-md-3">
+                    <div class="card card-block">
+                        <h4 class="card-title"><?php the_title(); ?></h4>
+                        <p class="card-text"><?php echo get_field("short_desc"); ?></p>
+                        <a href="javascript:void(0)" class="card-link"><?php echo get_the_date(); ?></a>
+                        <a href="<?php the_permalink(); ?>" class="card-link">Ver más.</a>
+                    </div>
+                </div>
+            <?php endif; ?>
         <?php endwhile; ?>
     </div>
 </div>
